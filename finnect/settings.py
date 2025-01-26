d0db6e1c2274d4388563f5fb1a8737f7c6c28e56
Thalison Fernandes <thalissfernandes99@gmail.com>
2025-01-25 21:25:08 -0300

    chore: update environment and settings for PostgreSQL database usage

--- finnect/settings.py
+++ finnect/settings.py
@@ -104,8 +104,12 @@ WSGI_APPLICATION = "finnect.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": getenv("POSTGRES_DB"),
+        "USER": getenv("POSTGRES_USER"),
+        "PASSWORD": getenv("POSTGRES_PASSWORD"),
+        "HOST": getenv("POSTGRES_HOST"),
+        "PORT": getenv("POSTGRES_PORT"),
     }
 }
 
