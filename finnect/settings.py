f3cb2190446985e25d33fcd3a2ef3f55ef79586e
Thalison Fernandes <thalissfernandes99@gmail.com>
2025-02-01 16:23:23 -0300

    feat: add custom header middleware for authenticated user emails

--- finnect/settings.py
+++ finnect/settings.py
@@ -77,6 +77,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "finnect.apps.userauth.middlewares.CustomHeaderMiddleware",
 ]
 
 ROOT_URLCONF = "finnect.urls"
