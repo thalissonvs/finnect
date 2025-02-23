c295cf7c2526cbd556f3b18a91c6e98d3db37c73
Thalison Fernandes <thalissfernandes99@gmail.com>
2025-02-23 17:13:39 -0300

    feat: add ContentView model and admin interface for tracking views

--- finnect/apps/common/admin.py
+++ finnect/apps/common/admin.py
@@ -1,3 +1,74 @@
 from django.contrib import admin
+from typing import Any
+from django.contrib.contenttypes.admin import GenericTabularInline
+from django.http import HttpRequest
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import ContentView
+
+
+@admin.register(ContentView)
+class ContentViewAdmin(admin.ModelAdmin):
+    list_display = [
+        'content_object',
+        'content_type',
+        'user',
+        'viewer_ip',
+        'last_viewed',
+        'created_at',
+    ]
+    list_filter = ['content_type', 'user', 'created_at']
+    date_hierarchy = 'last_viewed'
+    readonly_fields = [
+        'content_type',
+        'object_id',
+        'content_object',
+        'user',
+        'viewer_ip',
+        'created_at',
+        'updated_at',
+    ]
+    fieldsets = (
+        (None, {'fields': ('content_type', 'object_id', 'content_object')}),
+        (_('Viewer'), {'fields': ('user', 'viewer_ip', 'last_viewed')}),
+        (
+            _('Timestamps'),
+            {'fields': ('created_at', 'updated_at'), 'classes': ('collapse',)},
+        )
+    )
+
+    def has_add_permission(self, request: HttpRequest) -> bool:
+        """
+        Previne a adição de novas visualizações através do painel de administração.
+        """
+        return False
+    
+    def has_change_permission(self, request: HttpRequest, obj: Any = None) -> bool:
+        """
+        Impede a edição de visualizações existentes através do painel de administração.
+        """
+        return False
+    
+
+class ContentViewInline(GenericTabularInline):
+    model = ContentView
+    extra = 0
+    readonly_fields = [
+        'user',
+        'viewer_ip',
+        'last_viewed',
+        'created_at',
+    ]
+    can_delete = False
+
+    def has_add_permission(self, request: HttpRequest) -> bool:
+        """
+        Previne a adição de novas visualizações através do painel de administração.
+        """
+        return False
+    
+    def has_change_permission(self, request: HttpRequest, obj: Any = None) -> bool:
+        """
+        Impede a edição de visualizações existentes através do painel de administração.
+        """
+        return False
--- finnect/apps/common/models.py
+++ finnect/apps/common/models.py
@@ -1,3 +1,115 @@
-from django.db import models
+import uuid
+from typing import Any, Optional
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import IntegrityError, models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        # define que a classe é abstrata, ou seja, não será criada uma tabela
+        #  no banco de dados e sim será usada como modelo base para outras classes.
+        abstract = True
+
+
+class ContentView(TimeStampedModel):
+    """
+    Modelo para rastrear visualizações de diferentes tipos de conteúdo na aplicação.
+
+    Este modelo utiliza ContentType e GenericForeignKey para criar relacionamentos
+    genéricos com qualquer modelo da aplicação. Isso permite rastrear visualizações
+    de diferentes tipos de objetos (posts, produtos, etc) em uma única tabela.
+
+    Attributes:
+        content_type (ForeignKey): Referência ao modelo ContentType que identifica
+            o tipo do objeto sendo visualizado.
+        object_id (UUIDField): UUID do objeto específico sendo visualizado.
+        content_object (GenericForeignKey): Campo especial que combina content_type
+            e object_id para fornecer acesso direto ao objeto relacionado.
+        user (ForeignKey): Usuário opcional que realizou a visualização.
+
+    Exemplo de Uso:
+        >>> from django.contrib.contenttypes.models import ContentType
+        >>> # Registrando visualização de um post
+        >>> post = Post.objects.get(id='123e4567-e89b-12d3-a456-426614174000')
+        >>> ContentView.objects.create(
+        ...     content_type=ContentType.objects.get_for_model(Post),
+        ...     object_id=post.id,
+        ...     user=request.user
+        ... )
+        
+        >>> # Consultando visualizações
+        >>> # Todas as visualizações de um post específico
+        >>> views = ContentView.objects.filter(
+        ...     content_type=ContentType.objects.get_for_model(Post),
+        ...     object_id=post.id
+        ... )
+        
+        >>> # Visualizações por usuário
+        >>> user_views = ContentView.objects.filter(user=user)
+        
+        >>> # Acessando o objeto relacionado
+        >>> view = ContentView.objects.first()
+        >>> related_object = view.content_object  # Retorna o objeto visualizado
+    """
+    content_type = models.ForeignKey(
+        ContentType, on_delete=models.CASCADE, verbose_name=_('Content Type')
+    )
+    object_id = models.UUIDField(verbose_name=_('Object ID'))
+    content_object = GenericForeignKey('content_type', 'object_id')
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='content_views',
+        verbose_name=_('User')
+    )
+    viewer_ip = models.GenericIPAddressField(
+        null=True, blank=True, verbose_name=_('Viewer IP')
+    )
+    last_viewed = models.DateTimeField()
+
+    class Meta:
+        verbose_name = _('Content View')
+        verbose_name_plural = _('Content Views')
+        # Impede que um usuário com o mesmo IP registre visualizações repetidas
+        # no banco de dados para o mesmo objeto.
+        unique_together = ['content_type', 'object_id', 'user', 'viewer_ip']
+
+    def __str__(self) -> str:
+        return (
+            f'{self.content_type} viewed by {self.user.get_full_name() if self.user else "Anonymous"} '
+            f'from IP {self.viewer_ip}'
+        )
+    
+    @classmethod
+    def record_view(
+        cls,
+        content_object: Any,
+        user: Optional[User],
+        viewer_ip: Optional[str] = None
+    ) -> None:
+        content_type = ContentType.objects.get_for_model(content_object)
+        try:
+            view, created = cls.objects.get_or_create(
+                content_type=content_type,
+                object_id=content_object.id,
+                user=user,
+                viewer_ip=viewer_ip,
+                defaults={'last_viewed': timezone.now()}
+            )
+            if not created:
+                view.last_viewed = timezone.now()
+                view.save()
+            
+        except IntegrityError:
+            pass
+    
